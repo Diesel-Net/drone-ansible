2a2ec3edb3624df6bae931acd6c218c1a8cd98ac
Thomas Daley <tomdaley92@gmail.com>
2022-02-07 13:07:00 -0800

    clean up

--- tags.sh
+++ tags.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-#echo -n "latest$1,`git describe --tags | grep -ioP '\d+.\d+.\d+' | head -1`$1,`git describe --tags | grep -ioP '\d+.\d+' | head -1`$1,`git describe --tags | grep -ioP '\d+' | head -1`$1"
-echo -n "latest$1,`echo $2 | grep -ioP '\d+.\d+.\d+' | head -1`$1,`echo $2 | grep -ioP '\d+.\d+' | head -1`$1,`echo $2 | grep -ioP '\d+' | head -1`$1"
+echo -n "latest$2,`echo $1 | grep -ioP '\d+.\d+.\d+' | head -1`$2,`echo $1 | grep -ioP '\d+.\d+' | head -1`$2,`echo $1 | grep -ioP '\d+' | head -1`$2"
