d1b413369e2e2fdcbdc9e287914d430d7dbfc19b
Thomas Daley <tomdaley92@gmail.com>
2022-02-07 12:55:18 -0800

    debug

--- tags.sh
+++ tags.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #echo -n "latest$1,`git describe --tags | grep -ioP '\d+.\d+.\d+' | head -1`$1,`git describe --tags | grep -ioP '\d+.\d+' | head -1`$1,`git describe --tags | grep -ioP '\d+' | head -1`$1"
-echo -n "$1" | grep -oP '\d+.\d+.\d+' | head -1`$1,` | grep -ioP '\d+.\d+' | head -1`$1,` | grep -ioP '\d+' | head -1`$1"
+echo -n "latest$1,`echo $2 | grep -ioP '\d+.\d+.\d+' | head -1`$1,`echo $2 | grep -ioP '\d+.\d+' | head -1`$1,`echo $2 | grep -ioP '\d+' | head -1`$1"
