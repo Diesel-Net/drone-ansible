10bfcf9adb8ee5c6e9bbbdfdd6e08eeb499766b9
Thomas Daley <tomdaley92@gmail.com>
2022-07-12 21:33:05 -0700

    convert back to ansible-core

--- tags.sh
+++ tags.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo -n "latest$2,`echo $1 | grep -ioP '\d+.\d+.\d+' | head -1`$2,`echo $1 | grep -ioP '\d+.\d+' | head -1`$2,`echo $1 | grep -ioP '\d+' | head -1`$2"
+echo -n "latest$2,`echo $1 | grep -ioP '\d+.\d+' | head -1`$2,`echo $1 | grep -ioP '\d+' | head -1`$2"
